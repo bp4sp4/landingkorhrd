7b6a2c9e35f50eff31d0727b7a1e90c11b68fe4c
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2025-06-26 18:56:51 +0900

    update img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
 export default function Home() {
-  return <h1 className="text-center">안녕 나는 랜딩페이지!</h1>;
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-[#2a0a5c] to-[#6e2eb7] py-8">
+      <div className="max-w-lg mx-auto flex flex-col items-center">
+        <img
+          src="/001.png"
+          alt="사회복지사 자격증 안내"
+          className="shadow-xl w-full object-cover"
+        />
+        <img
+          src="/002.png"
+          alt="사회복지사2급자격증 설명"
+          className="r shadow-xl w-full object-cover"
+        />
+        <img
+          src="/003.png"
+          alt="사회복지사자격증 취득조건"
+          className=" shadow-xl w-full object-cover"
+        />
+        <img
+          src="/004.png"
+          alt="1:1 밀착 학습관리 시스템"
+          className="shadow-xl w-full object-cover"
+        />
+        <img
+          src="/005.png"
+          alt="한평생교육원에서!"
+          className=" shadow-xl w-full object-cover"
+        />
+      </div>
+    </main>
+  );
 }
